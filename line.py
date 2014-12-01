ee931c8e1d0bdad105942fa495c3b1d41a63607a
Rodrigo Morgado <rodrigomorgado28@gmail.com>
2014-12-01 21:00:22 -0200

    Colocando os coeficientes angulares e lineares da reta, que devem ser úteis para achar a distância até um determinado ponto

--- line.py
+++ line.py
@@ -6,6 +6,9 @@ class Line():
 		self.ponto_inicial = ponto_inicial
 		self.ponto_final = ponto_final
 
+		self.coeficienteAngular = (ponto_inicial.y - ponto_final.y)/(ponto_inicial.x - ponto_final.x)
+		self.coeficienteLinear = ponto_inicial.y - self.coeficienteAngular * ponto_inicial.x
+
 	def __str__(self):
 		return "Ponto Inicial: " + str(self.ponto_inicial) + "\nPonto Final: " + str(self.ponto_final)
 		
