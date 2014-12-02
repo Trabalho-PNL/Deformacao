ce42f350bb13c44b82e089992da8ace443fae195
gabriel-almeida <gabriel.almeida.cp2@gmail.com>
2014-12-02 08:30:51 -0200

    Adicionada funcao de distancia entre reta e ponto. Adicionada funcoes utilitarias em Point. Removido divisao por zero da Line

--- line.py
+++ line.py
@@ -5,10 +5,33 @@ class Line():
 	def __init__(self, ponto_inicial, ponto_final):
 		self.ponto_inicial = ponto_inicial
 		self.ponto_final = ponto_final
-
-		self.coeficienteAngular = (ponto_inicial.y - ponto_final.y)/(ponto_inicial.x - ponto_final.x)
-		self.coeficienteLinear = ponto_inicial.y - self.coeficienteAngular * ponto_inicial.x
-
+		
 	def __str__(self):
 		return "Ponto Inicial: " + str(self.ponto_inicial) + "\nPonto Final: " + str(self.ponto_final)
+
+	def distanciaPonto(self, ponto):
+		''' Calcula a distancia do segmento de reta a um dado ponto. Baseado em :
+		https://stackoverflow.com/questions/849211/shortest-distance-between-a-point-and-a-line-segment'''
 		
+		vetorDirecao = self.ponto_final - self.ponto_inicial
+		norma = vetorDirecao.produtoInterno(vetorDirecao)
+
+		if norma == 0.0:
+			return ponto.distancia(self.ponto_inicial) # reta degenerada em um ponto
+
+		t = (ponto - self.ponto_inicial).produtoInterno(vetorDirecao) / norma
+		
+		if t < 0.0:
+			return ponto.distancia(self.ponto_inicial)
+		elif t > 1.0:
+			return ponto.distancia(self.ponto_final)
+		else :
+			projecao = self.ponto_inicial + vetorDirecao * t
+			return ponto.distancia(projecao)
+
+if __name__ == "__main__":
+	l1 = Line(Point(0.0, 0.0), Point(0.0, 2.0))
+	l2 = Line(Point(0.0, 2.0), Point(0.0, 5.0))
+	p  = Point(2.0, 2.0)
+	print( l1.distanciaPonto(p) )
+	print( l2.distanciaPonto(p) )
--- point.py
+++ point.py
@@ -1,3 +1,5 @@
+import math
+
 class Point():
 	"""docstring for Python"""
 	def __init__(self, x, y):
@@ -7,3 +9,18 @@ class Point():
 	def __str__(self):
 	 return "x = " + str(self.x) + " y = " + str(self.y) 
 
+	def __sub__(self, other):
+		return Point(self.x - other.x, self.y - other.y)
+
+	def __add__(self, other):
+		return Point(self.x + other.x, self.y + other.y)
+	
+	def __mul__(self, other):
+		return Point(other*self.x, other*self.y)
+
+	def produtoInterno(self, other):
+		return self.x * other.x + self.y * other.y
+
+	def distancia(self, other):
+		direcao = self - other
+		return math.sqrt( direcao.produtoInterno(direcao) )
